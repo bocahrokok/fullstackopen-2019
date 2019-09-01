da4afe98807d3c481d4d08c92bfc31c9e4c40f8d
bocahrokok <indieunistudy@gmail.com>
2019-09-01 09:56:26 +0700

    com #1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,20 +1,44 @@
-import React, { useState } from 'react'
+import React  from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+const App = (props) => {
+  const { notes } = props
 
   return (
-    <div>{counter}</div>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
   )
 }
 
 ReactDOM.render(
-  <App />, 
+  <App notes={notes} />,
   document.getElementById('root')
 )
