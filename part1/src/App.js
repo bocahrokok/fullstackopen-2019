59275a79f5721dafadc6ac1307f2f1da3df33570
bocahrokok <indieunistudy@gmail.com>
2019-09-04 06:03:52 +0700

    com 1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,20 +1,44 @@
-import React from 'react'
-import Note from './Note'
+import React, { useState } from 'react'
+import Note from './Components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+
   const rows = () => notes.map(note =>
     <Note
       key={note.id}
       note={note}
     />
   )
-/////mantap
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {rows()}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>      
     </div>
   )
 }
