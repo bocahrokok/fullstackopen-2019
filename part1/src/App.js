93163650705419d05c867a6c5e42ba585fa0a955
bocahrokok <indieunistudy@gmail.com>
2019-09-06 00:11:41 +0700

    com

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,44 +1,22 @@
 import React, { useState } from 'react'
-import Note from './Components/Note'
+import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
 
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
-
-  const rows = () => notes.map(note =>
-    <Note
-      key={note.id}
-      note={note}
-    />
+  const rows = () => notes.map(note => 
+    <Note 
+    key={note.id}
+    note={note}
+    /> 
   )
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-  }
-
-
-  return (
+  return ( 
     <div>
       <h1>Notes</h1>
-      <ul>
+      <ul> 
         {rows()}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>      
     </div>
   )
 }
