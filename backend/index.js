01b00cbf003f2a4ef42a2acacdc513fa8ca85769
bocahrokok <indieunistudy@gmail.com>
2019-09-12 15:18:11 +0700

    com sore

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ let notes = [
 
 const app = http.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World')
+    res.end(JSON.stringify(notes))
 })
 
 const port = 3001
