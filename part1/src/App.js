7800e579afee32ccc4a80fcee6465d5d37cd1fdc
bocahrokok <indieunistudy@gmail.com>
2019-09-16 20:00:45 +0700

    com p2.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,34 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import axios from 'axios'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([]) 
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    console.log('effect')
+  console.log('render', notes.length, 'notes')
+
+  useEffect(() => {
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
-  }
-  
-  useEffect(hook, [])
-  
-  console.log('render', notes.length, 'notes')
+  }, [])
 
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
 
+  
+    const toggleImportanceOf = id => {
+      const url = `http://localhost:3001/notes/${id}`
+      const note = notes.find(n => n.id === id)
+      const changedNote = {...note, important: !note.important}
+
+      axios.put (url, changedNote).then(response => {
+        setNotes(notes.map(note => note.id !== id? note: response.data))
+      })
+      console.log(
+        'importance of ' + id + ' 5needs to be toggled'
+      )
+    }
+
   const rows = () => notesToShow.map(note =>
     <Note
       key={note.id}
       note={note}
+      toggleImportance={() => toggleImportanceOf(note.id)}
     />
   )
   
@@ -44,10 +56,16 @@ const App = () => {
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
       id: notes.length + 1,
+
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+        console.log(response);
+      })
   }
 
   return (
