cc9b89f53070f9645463264db0670c481129b27d
bocahrokok <indieunistudy@gmail.com>
2019-09-16 21:09:42 +0700

    comit 2d

--- part1/src/App.js
+++ part1/src/App.js
@@ -22,16 +22,21 @@ const App = () => {
     : notes.filter(note => note.important)
 
   
-     const toggleImportanceOf = id => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-  
-      noteService
-        .update(id, changedNote)
-          .then(returnedNote => {
-          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-        })
-    }
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error=>  {
+        alert(
+          `the note ' ${note.content}' was already deleted from the server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
 
   const rows = () => notesToShow.map(note =>
     <Note
