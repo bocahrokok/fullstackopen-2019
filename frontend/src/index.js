7a499f0b2fe70e885797c6256c92add0cc9d50d6
bocahrokok <indieunistudy@gmail.com>
2019-09-24 21:43:03 +0700

    rewrite from fullstack

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,20 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'  
 import './index.css'
 
-const http = require('http')
-
-const app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Hello World')
-})
-
-const port = 3001
-
-app.listen(port)
-console.log(`Server running on port ${port}`)
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
-
